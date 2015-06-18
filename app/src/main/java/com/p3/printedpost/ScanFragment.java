fe8215524cd17247c1913b0e51489b6224344d08
Igor Matos <igormattos.ea@gmail.com>
2015-06-17 21:45:58 -0300

    scan fragment

--- app/src/main/java/com/p3/printedpost/ScanFragment.java
+++ app/src/main/java/com/p3/printedpost/ScanFragment.java
@@ -2,17 +2,11 @@ package com.p3.printedpost;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 import com.google.zxing.ResultPoint;
 import com.journeyapps.barcodescanner.BarcodeCallback;
@@ -21,18 +15,16 @@ import com.journeyapps.barcodescanner.CompoundBarcodeView;
 
 import java.util.List;
 
+
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ScanFragment extends Fragment {
     private CompoundBarcodeView barcodeView;
-    private RelativeLayout ll;
-    private FragmentActivity fa;
-
-    public ScanFragment() {
-    }
 
     private BarcodeCallback callback = new BarcodeCallback() {
+
         @Override
         public void barcodeResult(BarcodeResult result) {
             if (result.getText() != null) {
@@ -48,38 +40,62 @@ public class ScanFragment extends Fragment {
         }
     };
 
+
+    public ScanFragment() {
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        fa = super.getActivity();
-        return (RelativeLayout)inflater.inflate(R.layout.fragment_scan, container, false);
+
+
+
+        return inflater.inflate(R.layout.fragment_scan, container, false);
 
 
     }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        Log.v("TESTE", "Initializing sounds...");
+        super.onActivityCreated(savedInstanceState);
 
         barcodeView = (CompoundBarcodeView) getView().findViewById(R.id.barcode_scanner);
         barcodeView.setStatusText(getString(R.string.reader_promt));
         barcodeView.decodeContinuous(callback);
 
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
 
+        barcodeView.resume();
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
 
+        barcodeView.pause();
+    }
+
+    public void pause(View view) {
+        barcodeView.pause();
+    }
+
+    public void resume(View view) {
+        barcodeView.resume();
     }
 
 
 
-
-    private boolean onKeyDown(int keyCode, KeyEvent event) {
-        return barcodeView.onKeyDown(keyCode, event) || fa.onKeyDown(keyCode, event);
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeView.onKeyDown(keyCode, event) || getActivity().onKeyDown(keyCode, event);
     }
 
     public void previousActivity(View view) {
         Log.v("log", "clicou");
-    };
+    }
 
 
 }
