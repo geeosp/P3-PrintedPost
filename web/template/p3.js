0851d262dd8daa1bd107944a9c36d4d673a717e1
Igor Matos <igormattos.ea@gmail.com>
2015-07-14 11:57:33 -0300

    utilizando mdl agr

--- web/template/p3.js
+++ web/template/p3.js
@@ -14,10 +14,11 @@
           success: function (object) {
               $(".success").show();
               console.log("dentro do test");
-              document.getElementById("qrCode").src = ("http://chart.googleapis.com/chart?cht=qr&chs=400x400&choe=UTF-8&chld=H|0&chl=" + object.id);
-              document.getElementById("qrDownload").href = ("http://chart.googleapis.com/chart?cht=qr&chs=400x400&choe=UTF-8&chld=H|0&chl=" + object.id);
-
-
+              // document.getElementById("qrCode").src = ("http://chart.googleapis.com/chart?cht=qr&chs=400x400&choe=UTF-8&chld=H|0&chl=" + object.id);
+              document.getElementById("qrDownload").href = ("http://chart.googleapis.com/chart?cht=qr&chs=800x800&choe=UTF-8&chld=H|0&chl=" + object.id);
+              document.getElementById("tTitle").innerHTML= '' + object.get("title");
+              document.getElementById("pResumo").innerHTML='Resumo: ' + object.get("excerpt");
+              $("#qrPreview").css('background-image', 'url(http://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=H|0&chl=' +  object.id +')' );
           },
           error: function (model, error) {
               $(".error").show();
